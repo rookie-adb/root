0a0206d3d16c38cd4ace0d0b9166a3ab5f2f051c
lucky <602992172@qq.com>
2023-08-02 20:46:02 +0800

    aaa

---
+++ main.js
@@ -0,0 +1,44 @@
+import App from './App'
+// #ifndef VUE3
+import Vue from 'vue'
+import './uni.promisify.adaptor'
+import {$http} from '@escook/request-miniprogram'
+
+uni.$http = $http
+$http.baseUrl='https://api-hmugo-web.itheima.net'
+//请求拦截器
+$http.beforeRequest = function(options){
+	uni.showLoading({
+		title:'数据加载中...'
+	})
+}
+//响应拦截器
+$http.afterRequest = function()
+{
+	uni.hideLoading()
+}
+//封装弹框
+uni.$showMsg = function(title = '数据请求失败', duration = 1500){
+	uni.showToast({
+		title,
+		duration,
+		icon:'none'
+	})
+}
+Vue.config.productionTip = false
+App.mpType = 'app'
+const app = new Vue({
+  ...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
